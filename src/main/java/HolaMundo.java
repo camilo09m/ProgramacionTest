466ed6e817900332d61ee1a4d9e4ab50d3e8610f
camilo09m <c.nanco01@ufromail.cl>
2021-09-23 21:27:02 -0300

    segundo commit

--- src/main/java/HolaMundo.java
+++ src/main/java/HolaMundo.java
@@ -2,4 +2,7 @@ public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("hola gente xd");
     }
+    public static void metodoPruebaCamilo(){
+        System.out.println("klk");
+    }
 }

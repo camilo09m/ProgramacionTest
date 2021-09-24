53a099bc496e0f1edd6e001066b4aa4b74a5f50f
ianprogra1 <https://github.com/iancicarelli/ian.git>
2021-09-23 21:33:09 -0300

    3

--- src/main/java/HolaMundo.java
+++ src/main/java/HolaMundo.java
@@ -1,8 +1,12 @@
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("hola gente xd");
+        int a = 0;
+        System.out.println("a");
     }
     public static void metodoPruebaCamilo(){
+
         System.out.println("klk");
     }
+
 }

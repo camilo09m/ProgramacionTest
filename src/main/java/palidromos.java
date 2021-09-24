a2f54d4d206379f0c687551b1389ac3b97eea3c1
Gonzalo <g.caniupan01@ufromail.cl>
2021-09-23 20:49:16 -0400

    commit gonzalo

--- src/main/java/palidromos.java
+++ src/main/java/palidromos.java
@@ -2,10 +2,8 @@
 
 public class palidromos {
     public static void main (String []args){
-        String[] cadenas = { "La ruta natural", "Esto no es", "Hola", "Sol", "Ana", "Oro", "Oso",};
-        for (String cadena : cadenas) {
-            System.out.println("¿'" + cadena + "' es palíndromo? " + esPalindromo(cadena));
-        }
+        String[] cadenas = { "La ruta natural", "Esto no es", "Hola", "", "Ana", "Oro", "Oso",};
+        imprimirCadena(cadenas);
 
     }
 
@@ -15,6 +13,31 @@ public class palidromos {
 
         String invertida = new StringBuilder(cadena).reverse().toString();
         return invertida.equals(cadena);
+    }
+    public static void verificacionString(){
+
+
+    }
+    public static void menu() {
+        System.out.println(".........{Ingresa Una Opccion }........");
+        System.out.println("[1] opcion 1");
+        System.out.println("[2] opcion 2");
+        System.out.println("[3] opcion 3");
+        System.out.println("[4] opcion 4");
+
+    }
+    public static void imprimirCadena(String [] cadenas){
+        for (String cadena : cadenas) {
+
+            if (cadena == "") {
+                System.out.println("Cadena vacia");
+            }else{
+                System.out.println("¿'" + cadena + "' es palíndromo? " + esPalindromo(cadena));
+
+            }
+
+        }
+
     }
 
 }
--- src/test/java/palidromosTest.java
+++ src/test/java/palidromosTest.java
@@ -3,6 +3,23 @@ import static org.junit.jupiter.api.Assertions.*;
 class palidromosTest {
 
     @org.junit.jupiter.api.Test
-    void esPalindromo() {
+    void esPalindromo0() {
+        String cadena= "aca";
+        assertEquals(true,palidromos.esPalindromo(cadena));
+    }
+    @org.junit.jupiter.api.Test
+    void esPalindromo1(){
+
+
+    }
+    @org.junit.jupiter.api.Test
+    void esPalindromo3() {
+        String cadena= "aaabccbaaa";
+        assertEquals(true,palidromos.esPalindromo(cadena));
+    }
+    @org.junit.jupiter.api.Test
+    void esPalindromo4() {
+        String cadena= "ahabccbaaa";
+        assertEquals(false,palidromos.esPalindromo(cadena));
     }
 }

dce2efd336fe6bda0d6f3f866586365b54ba93ea
Pincheira07 <m.pincheira07@ufromail.cl>
2021-09-23 21:21:53 -0300

    commit matias 1

--- src/main/java/HolaMundo.java
+++ src/main/java/HolaMundo.java
@@ -2,4 +2,8 @@ public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("hola gente xd");
     }
+
+    public static void metodoDePruebaMatias(){
+        System.out.println("Metodo de prueba Matias Pincheira");
+    }
 }
